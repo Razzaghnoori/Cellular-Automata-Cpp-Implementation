a7ea8ebc79ad4ee50b74b5df25eb3d10905388ab
Mohammad Razzaghnoori <razzaghnoori@gmail.com>
2021-07-19 17:47:52 +0200

    Add FastFlow Support

--- ca.cpp
+++ ca.cpp
@@ -5,11 +5,15 @@
 #include <ctime>
 #include <vector>
 #include <thread>
+#include <ff/ff.hpp>
+#include <ff/parallel_for.hpp>
 
 #include "config.h"
 #include "utimer.cpp"
 
 using namespace std;
+using namespace ff;
+
 
 vector<vector<int>> run_in_parallel_omp(vector<vector<int>> init_board, int num_row_threads=2, int num_col_threads=2, int num_iters=100, int radius=1)
     {
@@ -53,7 +57,7 @@ vector<vector<int>> run_sequentially(vector<vector<int>> init_board, int num_ite
         }
         auto elapsed = std::chrono::system_clock::now() - start;
         auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-        cout<<"Sequential task took " << musec << " to finish.\n";
+        cout<<"Sequential task took " << musec << " microsecond to finish.\n";
         return(init_board);
     }
 
@@ -69,12 +73,10 @@ auto compute_chunk(vector<vector<int>> &old_board, vector<vector<int>> &new_boar
 }
 
 vector<vector<int>> run_in_parallel(vector<vector<int>> init_board, int num_row_threads=2, int num_col_threads=2, int num_iters=100, int radius=1){
-    auto start = std::chrono::system_clock::now();
+    utimer u("Parallel task");
     pair<int, int> size(init_board.size(), init_board[0].size());
     vector<vector<int>> even_board = init_board;
     vector<vector<int>> odd_board = init_board;
-
-    // cout<<size.first << " " << num_row_threads << size.second << " "<< num_col_threads;
 
     int w_chunk_size = size.first/num_row_threads;
     int h_chunk_size = size.second/num_col_threads;
@@ -123,12 +125,43 @@ vector<vector<int>> run_in_parallel(vector<vector<int>> init_board, int num_row_
         for(thread& t: tids) t.join();
     }
 
-    auto elapsed = std::chrono::system_clock::now() - start;
-    auto musec = std::chrono::duration_cast<std::chrono::microseconds>(elapsed).count();
-    cout<<"Parallel task took " << musec << " to finish.\n";
-
     if(num_iters%2 == 0) return even_board;
     else return odd_board;
+}
+
+vector<vector<int>> run_in_parallel_ff(vector<vector<int>> init_board, int num_row_threads=2, int num_col_threads=2, int num_iters=100, int radius=1){
+    utimer timer("Parallel task using FastFlow");
+    pair<int, int> size(init_board.size(), init_board[0].size());
+    vector<vector<int>> even_board = init_board;
+    vector<vector<int>> odd_board = init_board;
+
+    ParallelFor outter_pf;
+    // ParallelFor inner_pf;
+
+
+    for(int iter=0; iter < num_iters; iter++){
+        outter_pf.parallel_for(0, size.first, [&](int i){
+#if 0
+            outter_pf.parallel_for(0, size.second, [&](int j){
+                if(iter%2 == 0){
+                    odd_board[i][j] = rules_func(extract_matrix(even_board, make_pair(i, j), radius));
+                } else{
+                    even_board[i][j] = rules_func(extract_matrix(odd_board, make_pair(i, j), radius));
+                }
+            }, num_col_threads);
+#else
+            for(int j=0; j<size.second; j++){
+                if(iter%2 == 0){
+                    odd_board[i][j] = rules_func(extract_matrix(even_board, make_pair(i, j), radius));
+                } else{
+                    even_board[i][j] = rules_func(extract_matrix(odd_board, make_pair(i, j), radius));
+                }
+            }
+#endif
+        }, num_row_threads * num_col_threads);
+    }
+
+    return (num_iters%2 == 0 ? even_board : odd_board);
 }
 
 int main() {
@@ -161,6 +194,8 @@ int main() {
 
     vector<vector<int>> seq_result = run_sequentially(board, num_iters);
     vector<vector<int>> par_result = run_in_parallel(board, num_row_threads, num_col_threads, num_iters);
+    vector<vector<int>> ff_par_result = run_in_parallel_ff(board, num_row_threads, num_col_threads, num_iters);
+
 
     // cout<<"--------------\n";
     // write_matrix(seq_result);
@@ -169,6 +204,7 @@ int main() {
     // write_matrix(par_result);
 
     cout << "Parallel and sequential outputs are " << (check_matrix_equality(seq_result, par_result) ? "" : "NOT ") << "equal.\n";
+    cout << "Parallel outputs using native c++ threads and FastFlow are " << (check_matrix_equality(par_result, ff_par_result) ? "" : "NOT ") << "equal.\n";
 
     return(0);
 }
