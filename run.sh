932444c5548a537f341060d244dd12922ef25496
Mohammad Razzaghnoori <razzaghnoori@gmail.com>
2021-07-29 15:25:28 +0200

    Test # Column Devisions Always =1

--- run.sh
+++ run.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-LEN=(30 100 300 1000)
-N_THR=(2 4 8)
-N_ITR=(2 8 32)
+LEN=(100 300 1000)
+N_THR=(2 4 8 16 32 64 128)
+N_ITR=(2 4 6)
 
 echo "Board Width, Board Height, N. Row Threads, N. Col Threads, N. Iters, Seq Time, Par Time, FF Time, Par Speed Up, FF Speed Up, Par Scalability, FF Scalability, Par Efficiency, FF Efficiency" > $1
 
 for l in ${LEN[@]}; do
     for nw in ${N_THR[@]}; do
         for itr in ${N_ITR[@]}; do
-            ./ca -w $l -h $l -i $itr -r $nw -c $nw -I 5 --rand >> $1
+            ./ca -w $l -h $l -i $itr -r $nw -c 1 -I 5 --rand >> $1
         done
     done
 done
